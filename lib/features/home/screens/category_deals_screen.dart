f412fad86c11283b41aaaa5b9709160aa22ac2cb
Haris Khan <haris90400@gmail.com>
2024-02-17 14:29:34 +0530

    Fetched Category Products

--- lib/features/home/screens/category_deals_screen.dart
+++ lib/features/home/screens/category_deals_screen.dart
@@ -1,3 +1,4 @@
+import 'package:amazon_clone_tutorial/common/widgets/loader.dart';
 import 'package:amazon_clone_tutorial/features/home/services/home_services.dart';
 import 'package:amazon_clone_tutorial/models/product.dart';
 import 'package:flutter/material.dart';
@@ -53,36 +54,74 @@ class _CategoryDealsScreenState extends State<CategoryDealsScreen> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-            alignment: Alignment.topLeft,
-            child: Text(
-              'Keep shopping for ${widget.category}',
-              style: const TextStyle(
-                fontSize: 20,
-              ),
+      body: productList == null
+          ? const Loader()
+          : Column(
+              children: [
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    'Keep shopping for ${widget.category}',
+                    style: const TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 170,
+                  child: GridView.builder(
+                    scrollDirection: Axis.horizontal,
+                    padding: const EdgeInsets.only(left: 15),
+                    itemCount: productList!.length,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 1,
+                      childAspectRatio: 1.4,
+                      mainAxisSpacing: 10,
+                    ),
+                    itemBuilder: (context, index) {
+                      final product = productList![index];
+                      return Column(
+                        children: [
+                          SizedBox(
+                            height: 130,
+                            child: DecoratedBox(
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.black12,
+                                  width: 0.5,
+                                ),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(10),
+                                child: Image.network(
+                                  product.images[0],
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            alignment: Alignment.topLeft,
+                            padding: const EdgeInsets.only(
+                              left: 0,
+                              top: 5,
+                              right: 15,
+                            ),
+                            child: Text(
+                              product.name,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
-          ),
-          SizedBox(
-            height: 170,
-            child: GridView.builder(
-              scrollDirection: Axis.horizontal,
-              padding: const EdgeInsets.only(left: 15),
-              itemCount: 10,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 1,
-                childAspectRatio: 1.4,
-                mainAxisSpacing: 10,
-              ),
-              itemBuilder: (context, index) {
-                return const Text('Hello World');
-              },
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
